987060cc5620a3a9ca1db8e0ebda9bf1db873180
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2023-10-17 15:27:49 +0200

    review
    Signed-off-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>

--- powsybl-build-tools/src/main/resources/powsybl-build-tools/liquibase-h2-init.sql
+++ powsybl-build-tools/src/main/resources/powsybl-build-tools/liquibase-h2-init.sql
@@ -1,3 +1,7 @@
+/* Liquibase H2 database init script
+ * Due to some differences between H2 and PostgreSQL dialects, even with H2 compatibility mode,
+ * we need to add some little "hacks" to not have errors like "unknow type".
+ */;
 CREATE DOMAIN IF NOT EXISTS JSONB AS JSON;
 /*CREATE DOMAIN IF NOT EXISTS OID AS BINARY LARGE OBJECT;*/
 /*create domain if not exist timestampstz as timestamp;*/
