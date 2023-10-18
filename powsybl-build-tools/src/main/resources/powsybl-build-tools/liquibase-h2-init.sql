77c047c7702d6ff8decb414f876292ebd8137d28
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2023-10-18 10:00:26 +0200

    review doc
    Signed-off-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>

--- powsybl-build-tools/src/main/resources/powsybl-build-tools/liquibase-h2-init.sql
+++ powsybl-build-tools/src/main/resources/powsybl-build-tools/liquibase-h2-init.sql
@@ -1,7 +1,9 @@
 /* Liquibase H2 database init script
  * Due to some differences between H2 and PostgreSQL dialects, even with H2 compatibility mode,
- * we need to add some little "hacks" to not have errors like "unknow type".
+ * we need to add some little "hacks" to not have errors like "unknown type".
  */;
+
+/** There is some datatypes H2 in compatibility mode don"t take into account, so we create a domain/alias of equivalent type **/;
 CREATE DOMAIN IF NOT EXISTS JSONB AS JSON;
 /*CREATE DOMAIN IF NOT EXISTS OID AS BINARY LARGE OBJECT;*/
 /*create domain if not exist timestampstz as timestamp;*/
